186b33c59bfb07979322b517e1489c08d357d9b7
domarshmello <domarshmello@outlook.com>
2018-10-14 22:28:15 +0800

    c++入门

---
+++ cmake-build-debug/CMakeFiles/Fisrt_c__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fisrt_c__.dir/main.cpp.o"
+  "Fisrt_c__.pdb"
+  "Fisrt_c__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fisrt_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<<"\n";
+
+    std::cout << "Hello, World!\n" << std::endl;
+
+    cout<<"hello domarshmello ";
+
+    return 0;
+}
